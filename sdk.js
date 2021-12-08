63a28801f8877be39f3071b0c96f427c7f3e58b1
gonpombo8 <gonzalo@decentraland.org>
2021-12-08 12:58:07 -0300

    remove no-namespace rule for sdk

--- sdk.js
+++ sdk.js
@@ -9,6 +9,7 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-floating-promises": 2,
     "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-namespace": "off",
     "@typescript-eslint/no-unused-vars": [
       "warn",
       {
